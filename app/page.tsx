70fbecee66c86c5d0ede68f366ee08c5a3c4b169
Angela Naseri <104205840+angnas@users.noreply.github.com>
2024-05-16 17:51:20 +0200

    fetch data

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
 import Image from "next/image";
+import Link from "next/link";
+import ProductCard from "./components/ProductCard";
 
 export default function Home() {
   return (
     <main>
       <h1>hello world</h1>
+      <h2>angela</h2>
+      <Link href="/users">USERS</Link>
+      <ProductCard />
     </main>
   );
 }
